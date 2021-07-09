01b6c10d0dbb5728b399cff6a1fd6c8cf2f4e747
robot <robot@208-pro4-10>
2021-07-09 11:28:12 +0900

    2번째 테스트

--- src/src/s20417_20210709/Gitjava.java
+++ src/src/s20417_20210709/Gitjava.java
@@ -4,7 +4,8 @@ public class Gitjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println("20417이요원");
+		System.out.println("두번째 테스트");
 
 	}
 
